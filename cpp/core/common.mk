7f70b38ffe165f3a3090902a57b1a8e14b81c72f
Clifford Hung <chung@rim.com>
2012-04-17 14:25:40 -0400

    Clean up the build and ensure "make hinstall" will copy headers to staging directory
    - Move contents of lib one directory up.
    - Move contents of test_app to tests.
    - Move headers to a public directory (preserving the directory structure).
    - Link statically to the cppunit library since the library path has both static
    and dynamic libraries in it.

---
+++ cpp/core/common.mk
@@ -0,0 +1,60 @@
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+#####################################
+# Preset make macros go here
+#####################################
+
+NAME=zxing
+SO_VERSION=2.0
+
+# Default installation path for make install target
+INSTALL_ROOT_nto=$(PROJECT_ROOT)/stage/target/qnx6
+
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/datamatrix
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/datamatrix/detector
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/datamatrix/decoder
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/oned
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/common
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/common/detector
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/common/reedsolomon
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/aztec
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/aztec/detector
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/aztec/decoder
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/qrcode
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/qrcode/detector
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/qrcode/decoder
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/multi
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/multi/qrcode
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/src/zxing/multi/qrcode/detector
+
+EXTRA_INCVPATH+=$(shell find $(PROJECT_ROOT)/public -type d)
+
+# Avoid error when linking the test program
+CCFLAGS += -fPIC
+
+define PINFO
+PINFO DESCRIPTION=ZXing barcode decoder library
+endef
+INSTALLDIR=usr/lib
+
+include $(MKFILES_ROOT)/qtargets.mk
+
+#####################################
+# Post-set make macros go here
+#####################################
+
+# Use the QNX build system's $(VARIANT_LIST) to determine whether to use
+# debugging libraries, etc.
+ifeq ($(filter g,$(VARIANT_LIST)),g)
+  CCFLAGS += -O0
+else
+  # Non-debug build.
+  #
+  # Enable optimizations suitable for modern C++.
+  CCFLAGS += -O3
+endif
